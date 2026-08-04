bfa8f622564e5aadf74448bd00528fd3809a8e9c
panuwat39 <panuwat39t@gmail.com>
2026-08-04 15:24:49 +0700

    feat: add user domain and api models

---
+++ internal/user/model/response.go
@@ -0,0 +1,21 @@
+package model
+
+import "time"
+
+type UserResponse struct {
+	ID        string    `json:"id"`
+	Name      string    `json:"name"`
+	Email     string    `json:"email"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
+
+func ToUserResponse(user User) UserResponse {
+	return UserResponse{
+		ID:        user.ID,
+		Name:      user.Name,
+		Email:     user.Email,
+		CreatedAt: user.CreatedAt,
+		UpdatedAt: user.UpdatedAt,
+	}
+}
